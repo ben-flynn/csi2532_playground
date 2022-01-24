805e0b414a81131393506de183651dd84783008b
ben-flynn <55192923+ben-flynn@users.noreply.github.com>
2022-01-24 18:28:46 -0500

    Added code

---
+++ Lab 1/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE voitures(
+    brand varchar(255),
+    year integer,
+    model varchar(255)
+);
---
+++ Lab 1/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO voitures
+VALUES('Ford', 2020, 'F-650'),
+('Tesla', 2020, 'Model S'),
+('Tesla', 2020, 'Model Y'),
+('Ford', 2020, 'Equinox'),
+('Jeep', 2020, 'Wrangler');
